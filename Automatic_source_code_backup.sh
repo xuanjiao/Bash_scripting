9871e309a826d57bcdd4760d7a952980ac4ea0ce
xuanjiao <skaterxjz@gmail.com>
2020-12-26 15:38:15 +0100

    add conments

--- Automatic_source_code_backup.sh
+++ Automatic_source_code_backup.sh
@@ -11,14 +11,12 @@ else
 	mkdir -p ${NEW_DIR}		# If the directory not exits, create the directory.
 fi
 
-echo "Copying ${ORI_DIR} to ${NEW_DIR}"
+echo "Back up files"
 cp ${ORI_DIR}/*.c ${NEW_DIR} # Copy files to the "/home/c_str" directory.
 echo "Done."
 
-echo "Compressing src_${MYD}.tgz.."
-
+echo "Compress files"
 mkdir -p ${COMP_DIR}
-
 cd ${NEW_DIR} && tar -zcvf src_${MYD}.tgz . # Compress files and copy them to the "/home/c_comp" directory.
 cp src_${MYD}.tgz ${COMP_DIR}
-
+echo "Done."
