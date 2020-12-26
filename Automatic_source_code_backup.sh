15b9f6e9a387a35c895d397ada1be6de4d236911
xuanjiao <skaterxjz@gmail.com>
2020-12-26 15:35:50 +0100

    auto backup finish

--- Automatic_source_code_backup.sh
+++ Automatic_source_code_backup.sh
@@ -2,6 +2,7 @@
 ORI_DIR=$1
 NEW_DIR=/home/pi/c_str/${1##*/}
 COMP_DIR=/home/pi/share
+MYD=`date +%Y_%m_%d`
 
 if [ -d ${NEW_DIR} ]
 then
@@ -11,12 +12,13 @@ else
 fi
 
 echo "Copying ${ORI_DIR} to ${NEW_DIR}"
-cp ${ORI_DIR}/*.c ${NEW_DIR} # Copy files
+cp ${ORI_DIR}/*.c ${NEW_DIR} # Copy files to the "/home/c_str" directory.
 echo "Done."
 
-echo "Compressing.."
+echo "Compressing src_${MYD}.tgz.."
 
 mkdir -p ${COMP_DIR}
 
-tar -zcf ${NEW_DIR} -- directory ${COMP_DIR}
+cd ${NEW_DIR} && tar -zcvf src_${MYD}.tgz . # Compress files and copy them to the "/home/c_comp" directory.
+cp src_${MYD}.tgz ${COMP_DIR}
 
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## 1.	Automatic source code backup
 1.  Copy all C files in the specified directory to the "/home/c_str" directory.
-2.  Compress these files into "srv_dd_mm_yy.tgz" and place them in the "/home/c_comp" directory.
+2.  Compress these files into "src_dd_mm_yy.tgz" and copy them to the "/home/c_comp" directory.
+
+## 2.   Automatic decompression and file classification
+1.  Unzip test.tar.gz. Copy all c files to the "c_dir" directory and all headle files into the "h_dir" directory.
+2.  Create a text file and list all c and headle files, count files.
