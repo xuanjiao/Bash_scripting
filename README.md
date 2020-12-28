eeaf6234d233a43240d7d4d2078c69eee4388ff9
xuanjiao <skaterxjz@gmail.com>
2020-12-28 14:59:09 +0100

    add some explanation

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Systems_programming (2020)
 
-## 1.	Automatic source code backup
+## Script 1.	Automatic source code backup
 1.  Copy all C files in the specified directory to the "/home/c_str" directory.
 2.  Compress these files into "src_dd_mm_yy.tgz" and copy them to the "/home/c_comp" directory.
 
-## 2.   Automatic decompression and file classification
+### Usage
+```
+sh code_backup.sh testDir
+```
+
+## Script 2. Automatic decompression and file classification
 1.  Unzip test.tar.gz. Copy all c files to the "c_dir" directory and all headle files into the "h_dir" directory.
 2.  Create a text file "dirinfo.txt" and list all c and headle files, count files.
+
+### Usage
+```
+sh classification.sh
+```
--- code_backup.sh
+++ code_backup.sh
@@ -2,21 +2,20 @@
 ORI_DIR=$1
 NEW_DIR=/home/pi/c_str/${1##*/}
 COMP_DIR=/home/pi/share
-MYD=`date +%Y_%m_%d`
+MYD=`date +%d_%m_%Y`
 
-if [ -d ${NEW_DIR} ]
-then
+if [ -d $NEW_DIR ]; then
 	rm ${NEW_DIR}/* 		# If the directory exists, empty this directory.
 else
-	mkdir -p ${NEW_DIR}		# If the directory not exits, create the directory.
+	mkdir -p $NEW_DIR		# If the directory not exits, create the directory.
 fi
 
 echo "Back up files"
 cp ${ORI_DIR}/*.c ${NEW_DIR} # Copy files to the "/home/c_str" directory.
 echo "Done."
 
+mkdir -p $COMP_DIR
 echo "Compress files"
-mkdir -p ${COMP_DIR}
-cd ${NEW_DIR} && tar -zcvf src_${MYD}.tgz . # Compress files and copy them to the "/home/c_comp" directory.
-cp src_${MYD}.tgz ${COMP_DIR}
+cd $NEW_DIR && tar -zcvf src_${MYD}.tgz . # Compress files and copy them to the "/home/c_comp" directory.
+cp src_${MYD}.tgz $COMP_DIR
 echo "Done."
